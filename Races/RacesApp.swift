2c0500a68cde78e56052ca949dd6b2da69a946a9
AntonR8 <129757394+AntonR8@users.noreply.github.com>
2025-05-03 17:05:43 +0300

    Initial Commit

---
+++ Races/RacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RacesApp.swift
+//  Races
+//
+//  Created by Антон Разгуляев on 03.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct RacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
